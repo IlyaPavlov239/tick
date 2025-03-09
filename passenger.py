9799d003274770afdd1a953a47349c86500d7e0e
IlyaPavlov239 <warstar08@gmail.com>
2025-03-10 00:52:51 +0300

    fhghf

--- passenger.py
+++
@@ -1,8 +0,0 @@
-class passenger:
-    def __init__(self, n, surn, arr, dep, t,tr):
-        self.arr=arr
-        self.dep=dep
-        self.t=int(t.split()[0])*60+int(t.split()[1])
-        self.n=n
-        self.surn=surn
-        self.tr=tr
--- ticket.py
+++
@@ -1,6 +0,0 @@
-class ticket:
-    def __init__(self, arr, dep, t, n):
-        self.arr=arr
-        self.dep=dep
-        self.t=int(t.split()[0])*60+int(t.split()[1])
-        self.n=n
